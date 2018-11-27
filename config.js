c36a128a33d12e3e1a5a1f0f85062b2434acaaa7
Leszek Macura <leszek@macura.co>
2018-11-27 21:48:48 +0100

    Zadanie 17_07 gotowe ale...

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '787624853141-o1tkl1jj1ghld5fbg34vmt2tqm6m44mf.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'wuKQbW4Y7cFNOKUqUdvrpjoD',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback' 
+};
